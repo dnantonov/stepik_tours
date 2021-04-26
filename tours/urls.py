1237b2ef6b860def0bbfe683712f8e38374186c1
kodoku2010 <dnantonov@stud.etu.ru>
2021-04-26 21:18:19 +0300

    Re-write views in Class-based style

--- tours/urls.py
+++ tours/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
-# from .views import MainView, DepartureView, TourView
-from .views import index, departure_view, tour_view
+from .views import MainView, DepartureView, TourView
+
 
 urlpatterns = [
-    # path('', MainView.as_view()),
-    # path('departure/<str:departure>/', DepartureView.as_view()),
-    # path('tour/<int:id>/', TourView.as_view()),
-    path('', index, name="index"),
-    path('departure/<str:departure>/', departure_view, name="departure"),
-    path('tour/<int:id>/', tour_view, name="tour"),
+    path('', MainView.as_view(), name="index"),
+    path('departure/<str:departure>/', DepartureView.as_view(), name="departure"),
+    path('tour/<int:id>/', TourView.as_view(), name="tour"),
 ]
--- tours/views.py
+++ tours/views.py
@@ -1,55 +1,44 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import View
 
 from .data import *
 
 
-def index(request):
-    context = {
-        'title': title,
-        'subtitle': subtitle,
-        'description': description,
-        'departures': departures,
-        'tours': tours
-    }
-    return render(request, 'tours/index.html', context)
+class MainView(View):
+    def get(self, request):
+        context = {
+            'title': title,
+            'subtitle': subtitle,
+            'description': description,
+            'departures': departures,
+            'tours': tours
+        }
+        return render(request, 'tours/index.html', context)
 
 
-def departure_view(request, departure):
-    return render(request, 'tours/departure.html')
+class DepartureView(View):
+    def get(self, request, departure):
+        return render(request, 'tours/departure.html')
 
 
-def tour_view(request, id):
-    tour = tours[id]
-    departure = departures[tours[id]['departure']]
-    nights = tours[id]['nights']
-    country = tours[id]['country']
-    picture = tours[id]['picture']
-    description = tours[id]['description']
-    price = tours[id]['price']
+class TourView(View):
+    def get(self, request, id):
+        tour = tours[id]
+        departure = departures[tours[id]['departure']]
+        nights = tours[id]['nights']
+        country = tours[id]['country']
+        picture = tours[id]['picture']
+        description = tours[id]['description']
+        price = tours[id]['price']
 
-    context = {
-        'tour': tour,
-        'departure': departure,
-        'nights': nights,
-        'country': country,
-        'picture': picture,
-        'description': description,
-        'price': price
-    }
-
-    return render(request, 'tours/tour.html', context)
-
-
-
-# class MainView(TemplateView):
-#     template_name = 'tours/index.html'
-#
-#
-# class DepartureView(TemplateView):
-#     template_name = 'tours/departure.html'
-#
-#
-# class TourView(TemplateView):
-#     template_name = 'tours/tour.html'
+        context = {
+            'tour': tour,
+            'departure': departure,
+            'nights': nights,
+            'country': country,
+            'picture': picture,
+            'description': description,
+            'price': price
+        }
+        return render(request, 'tours/tour.html', context)
 
