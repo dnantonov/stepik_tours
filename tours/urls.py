003988f170d837c9d3548bb7be03aab2f4220cb5
kodoku2010 <dnantonov@stud.etu.ru>
2021-04-27 14:48:07 +0300

    Finish departure and tour page

--- tours/urls.py
+++ tours/urls.py
@@ -4,7 +4,7 @@ from .views import MainView, DepartureView, TourView
 
 
 urlpatterns = [
-    path('', MainView.as_view(), name="index"),
-    path('departure/<str:departure>/', DepartureView.as_view(), name="departure"),
-    path('tour/<int:id>/', TourView.as_view(), name="tour"),
+    path('', MainView.as_view(), name='index'),
+    path('departure/<str:departure>/', DepartureView.as_view(), name='departure'),
+    path('tour/<int:id>/', TourView.as_view(), name='tour'),
 ]
--- tours/views.py
+++ tours/views.py
@@ -1,44 +1,68 @@
+import random
 from django.shortcuts import render
 from django.views.generic import View
 
-from .data import *
+from .data import title, subtitle, description, departures, tours
 
 
 class MainView(View):
+    # главная страница
     def get(self, request):
+        random_tours = sorted(tours.items(), key=lambda x: random.random())[:6]
         context = {
             'title': title,
             'subtitle': subtitle,
             'description': description,
             'departures': departures,
-            'tours': tours
+            'tours': random_tours
         }
         return render(request, 'tours/index.html', context)
 
 
-class DepartureView(View):
-    def get(self, request, departure):
-        return render(request, 'tours/departure.html')
-
-
 class TourView(View):
+    # просмотр определенного тура
     def get(self, request, id):
-        tour = tours[id]
-        departure = departures[tours[id]['departure']]
-        nights = tours[id]['nights']
-        country = tours[id]['country']
-        picture = tours[id]['picture']
-        description = tours[id]['description']
-        price = tours[id]['price']
-
         context = {
-            'tour': tour,
-            'departure': departure,
-            'nights': nights,
-            'country': country,
-            'picture': picture,
-            'description': description,
-            'price': price
+            'tour': tours[id],
+            'departure': departures[tours[id]['departure']],
+            'nights': tours[id]['nights'],
+            'country': tours[id]['country'],
+            'picture': tours[id]['picture'],
+            'description': tours[id]['description'],
+            'price': tours[id]['price'],
+            'departures': departures,
         }
         return render(request, 'tours/tour.html', context)
 
+
+class DepartureView(View):
+    # выезд из определенного города
+    def get(self, request, departure):
+        # найдем все туры которые подходят по запросу departure=departure
+        departure_tours = {}
+        for key, tour in tours.items():
+            if tour['departure'] == departure:
+                departure_tours[key] = tour
+        # создадим переменные и найдем туры с наименьшей и наибольшей ценой и
+        # c наименьшим и наибольшим количеством дней в туре
+        prices = []
+        nights = []
+        for min_tour in departure_tours.values():
+            prices.append(min_tour['price'])
+            nights.append(min_tour['nights'])
+        min_price = min(prices)
+        max_price = max(prices)
+
+        min_nights = min(nights)
+        max_nights = max(nights)
+  
+        context = {
+            'departure': departures[departure],
+            'tours': departure_tours,
+            'departures': departures,
+            'min_price': min_price,
+            'max_price': max_price,
+            'min_nights': min_nights,
+            'max_nights': max_nights,
+        }
+        return render(request, 'tours/departure.html', context)
